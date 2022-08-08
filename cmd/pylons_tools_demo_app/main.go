d1d1354b9f5e27ef89ccf7fda289a290584122ac
sine/cosine/tangent <zfshmn@gmail.com>
2022-08-08 16:28:00 -0700

    as close to a finished product as we're gonna get absent a saner way to interact w/ pylonscli

--- cmd/pylons_tools_demo_app/main.go
+++ cmd/pylons_tools_demo_app/main.go
@@ -35,13 +35,17 @@ var coinRegex = regexp.MustCompile(`  - key: coins\n    value: "(.*?)"\n`)
 
 var shardRegex = regexp.MustCompile(`  - key: shards\n    value: "(.*?)"\n`)
 
+// this is wild and broken, but this does in fact retrieve the exec id atm
+var execRegex = regexp.MustCompile(`  - key: itemID\n      value: (.*?)\n`)
+
 var itemIdRegex = regexp.MustCompile(`id: (.*?)\n`)
+
+var completedRegex = regexp.MustCompile(`completed: (.*?)\n`)
 
 func main() {
 	setLocalAccount()
 	generateCharacter()
 	for !gameEnded {
-		checkCharacter()
 		if swordLv == 0 {
 			fmt.Printf("You have %s/20 HP remaining. You are unarmed.\n\n", strconv.Itoa(curHp))
 		} else {
@@ -62,19 +66,19 @@ func main() {
 			case "1\n":
 				fightGoblin()
 			case "2\n":
-				//fightTroll()
+				fightTroll()
 			case "3\n":
-				//fightDragon()
+				fightDragon()
 			case "4\n":
-				//buySword()
+				buySword()
 			case "5\n":
-				//upgradeSword()
+				upgradeSword()
 			case "6\n":
-				//rest1()
+				rest1()
 			case "7\n":
-				//rest2()
+				rest2()
 			case "8\n":
-				//rest3()
+				rest3()
 			case "9\n":
 				gameEnded = true
 			}
@@ -122,11 +126,149 @@ func generateCharacter() {
 	dat = execQueryCmd([]string{"query", "tx", hash})
 	addr = addrRegex.FindStringSubmatch(dat)[1]
 	dat = execQueryCmd([]string{"query", "pylons", "list-item-by-owner", addr})
-	characterId = itemIdRegex.FindAllStringSubmatch(dat, -1)[1][1]
+	matches := itemIdRegex.FindAllStringSubmatch(dat, -1)
+	// The last character in the list will be the most recently created.
+	// We really need a way to handle this data w/o doing inane things w/ regex tho
+	characterId = matches[len(matches)-2][1]
+	checkCharacter()
 }
 
 func fightGoblin() {
+	println("Fighting a goblin...")
 	execTxCmd([]string{"tx", "pylons", "execute-recipe", "appTestCookbook", "RecipeTestAppFightGoblin", "0", fmt.Sprintf(`["%s"]`, characterId), "[]", "--from", localAccount})
+	println("Victory!")
+	var lastHp = curHp
+	var lastCoins = coins
+	checkCharacter()
+	if lastHp != curHp {
+		fmt.Printf("Took %s damage!\n", strconv.Itoa(lastHp-curHp))
+	}
+	if lastCoins != coins {
+		fmt.Printf("Found %s coins!\n", strconv.Itoa(coins-lastCoins))
+	}
+}
+
+func fightTroll() {
+	println("Fighting a troll...")
+	if swordLv < 1 {
+		execTxCmd([]string{"tx", "pylons", "execute-recipe", "appTestCookbook", "RecipeTestAppFightTrollUnarmed", "0", fmt.Sprintf(`["%s"]`, characterId), "[]", "--from", localAccount})
+		println("Defeat...")
+		var lastHp = curHp
+		var lastCoins = coins
+		checkCharacter()
+		if lastHp != curHp {
+			fmt.Printf("Took %s damage!\n", strconv.Itoa(lastHp-curHp))
+		}
+		if lastCoins != coins {
+			fmt.Printf("Found %s coins!\n", strconv.Itoa(coins-lastCoins))
+		}
+	} else {
+		execTxCmd([]string{"tx", "pylons", "execute-recipe", "appTestCookbook", "RecipeTestAppFightTrollArmed", "0", fmt.Sprintf(`["%s"]`, characterId), "[]", "--from", localAccount})
+		println("Victory!")
+		var lastHp = curHp
+		var lastCoins = coins
+		checkCharacter()
+		if lastHp != curHp {
+			fmt.Printf("Took %s damage!\n", strconv.Itoa(lastHp-curHp))
+		}
+		if lastCoins != coins {
+			fmt.Printf("Found %s coins!\n", strconv.Itoa(coins-lastCoins))
+		}
+	}
+}
+
+func fightDragon() {
+	println("Fighting a dragon...")
+	if swordLv < 2 {
+		execTxCmd([]string{"tx", "pylons", "execute-recipe", "appTestCookbook", "RecipeTestAppFightDragonUnarmed", "0", fmt.Sprintf(`["%s"]`, characterId), "[]", "--from", localAccount})
+		println("Defeat...")
+		var lastHp = curHp
+		var lastCoins = coins
+		checkCharacter()
+		if lastHp != curHp {
+			fmt.Printf("Took %s damage!\n", strconv.Itoa(lastHp-curHp))
+		}
+		if lastCoins != coins {
+			fmt.Printf("Found %s coins!\n", strconv.Itoa(coins-lastCoins))
+		}
+	} else {
+		execTxCmd([]string{"tx", "pylons", "execute-recipe", "appTestCookbook", "RecipeTestAppFightDragonArmed", "0", fmt.Sprintf(`["%s"]`, characterId), "[]", "--from", localAccount})
+		println("Victory!")
+		var lastHp = curHp
+		var lastCoins = coins
+		checkCharacter()
+		if lastHp != curHp {
+			fmt.Printf("Took %s damage!\n", strconv.Itoa(lastHp-curHp))
+		}
+		if lastCoins != coins {
+			fmt.Printf("Found %s coins!\n", strconv.Itoa(coins-lastCoins))
+		}
+	}
+}
+
+func buySword() {
+	if swordLv > 0 {
+		println("You already have a sword")
+	} else if coins < 50 {
+		println("You need 50 coins to buy a sword")
+	} else {
+		execTxCmd([]string{"tx", "pylons", "execute-recipe", "appTestCookbook", "RecipeTestAppBuySword", "0", fmt.Sprintf(`["%s"]`, characterId), "[]", "--from", localAccount})
+		println("Bought a sword!")
+		var lastCoins = coins
+		checkCharacter()
+		if lastCoins != coins {
+			fmt.Printf("Spent %s coins!\n", strconv.Itoa(lastCoins-coins))
+		}
+	}
+}
+
+func upgradeSword() {
+	if swordLv > 1 {
+		println("You already have an upgraded sword")
+	} else if coins < 50 {
+		println("You need 5 shards to upgrade your sword")
+	} else {
+		execTxCmd([]string{"tx", "pylons", "execute-recipe", "appTestCookbook", "RecipeTestAppUpgradeSword", "0", fmt.Sprintf(`["%s"]`, characterId), "[]", "--from", localAccount})
+		println("Upgraded your sword!")
+		var lastShards = shards
+		checkCharacter()
+		if lastShards != shards {
+			fmt.Printf("Spent %s shards!\n", strconv.Itoa(lastShards-shards))
+		}
+	}
+}
+
+func rest1() {
+	println("Resting...")
+	execDelayedTxCmd([]string{"tx", "pylons", "execute-recipe", "appTestCookbook", "RecipeTestAppRest25", "0", fmt.Sprintf(`["%s"]`, characterId), "[]", "--from", localAccount})
+	println("Done!")
+	var lastHp = curHp
+	checkCharacter()
+	if lastHp != curHp {
+		fmt.Printf("Recovered %s HP!\n", strconv.Itoa(curHp-lastHp))
+	}
+}
+
+func rest2() {
+	println("Resting...")
+	execDelayedTxCmd([]string{"tx", "pylons", "execute-recipe", "appTestCookbook", "RecipeTestAppRest50", "0", fmt.Sprintf(`["%s"]`, characterId), "[]", "--from", localAccount})
+	println("Done!")
+	var lastHp = curHp
+	checkCharacter()
+	if lastHp != curHp {
+		fmt.Printf("Recovered %s HP!\n", strconv.Itoa(curHp-lastHp))
+	}
+}
+
+func rest3() {
+	println("Resting...")
+	execDelayedTxCmd([]string{"tx", "pylons", "execute-recipe", "appTestCookbook", "RecipeTestAppRest100", "0", fmt.Sprintf(`["%s"]`, characterId), "[]", "--from", localAccount})
+	println("Done!")
+	var lastHp = curHp
+	checkCharacter()
+	if lastHp != curHp {
+		fmt.Printf("Recovered %s HP!\n", strconv.Itoa(curHp-lastHp))
+	}
 }
 
 func execQueryCmd(args []string) string {
@@ -153,5 +295,35 @@ func execTxCmd(args []string) string {
 	io.WriteString(stdin, "y\n")
 	cmd.Wait()
 	time.Sleep(time.Second * 5)
+	return outb.String()
+}
+
+func execDelayedTxCmd(args []string) string {
+	args[len(args)-1] = strings.TrimSpace(args[len(args)-1])
+	cmd := exec.Command("pylonsd", args...)
+	var outb bytes.Buffer
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = &outb
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		fmt.Println(err)
+	}
+	cmd.Start()
+	io.WriteString(stdin, "y\n")
+	cmd.Wait()
+	time.Sleep(time.Second * 5)
+	dat := outb.String()
+	hash := dat[len(dat)-65 : len(dat)-1]
+	dat = execQueryCmd([]string{"query", "tx", hash})
+	execId := execRegex.FindStringSubmatch(dat)[1]
+
+	var escaped = false
+
+	for !escaped {
+		dat := execQueryCmd([]string{"query", "pylons", "get-execution", execId})
+		escaped = completedRegex.FindStringSubmatch(dat)[1] == "true"
+		println("...")
+	}
+
 	return outb.String()
 }
